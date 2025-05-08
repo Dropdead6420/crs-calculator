56d811e7b9d49aa98d263fb1ee58fca528edba52
Saurabh <saurabhkumarmehto@gmail.com>
2025-05-08 10:01:50 +0530

    Removed unnecessary lines

--- backend/src/index.js
+++ backend/src/index.js
@@ -88,31 +88,4 @@ app.use("/api/admin/noc-job", adminNOCTeerRouter);
 const testing = require("./routes/questionRoutes")
 app.use("/api/testing", testing);
 
-// const agePointsRouter = require("./routes/AgePoints.route");
-// app.use("/super-admin/age_points", agePointsRouter);
-
-// const productRouter = require("./routes/product.route");
-// app.use("/api/product", productRouter);
-
-// const categoryRouter = require("./routes/category.route");
-// app.use("/api/category", categoryRouter);
-
-// // const serviceRoute = require("./routes/product.route");
-// // app.use("/api/product", serviceRouter);
-
-// const adminProductRouter = require("./routes/adminProduct.route");
-// app.use("/api/admin/product", adminProductRouter);
-
-// const cartRouter = require("./routes/cart.route");
-// app.use("/api/cart", cartRouter);
-
-// const cartItemRouter = require("./routes/cartItem.route");
-// app.use("/api/cart_items", cartItemRouter);
-
-// const orderRouter = require("./routes/order.route");
-// app.use("/api/order", orderRouter);
-
-// const adminOrderRouter = require("./routes/adminOrder.route");
-// app.use("/api/admin/order", adminOrderRouter);
-
 module.exports = app;
