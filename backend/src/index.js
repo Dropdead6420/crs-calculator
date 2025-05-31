b049064dbd94c24880c9581aa21f3e5c2ac4a4e1
Saurabh <saurabhkumarmehto@gmail.com>
2025-05-31 16:51:54 +0530

    Refactor index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,59 +7,8 @@ app.use(express.json());
 app.use(cors())
 
 app.get("/", (req, res) => {
-    return res.status(200).json({ title: "HexBytes", message: "CRM Calculator", Status: true });
+  return res.status(200).json({ title: "HexBytes", message: "CRS & FSWP Calculator API", Status: true });
 })
-
-const authRouter = require("./routes/Auth.route");
-app.use("/api/admin", authRouter);
-
-const maritalStatusOptionRouter = require("./routes/MaritalStatusOption.route");
-app.use("/api/marital-status", maritalStatusOptionRouter);
-
-const adminMaritalStatusOptionRouter = require("./routes/AdminMaritalStatusOption.route");
-app.use("/api/admin/marital-status", adminMaritalStatusOptionRouter);
-
-const agePointRouter = require("./routes/AgePoints.route");
-app.use("/api/age-points", agePointRouter);
-
-const adminAgePointRouter = require("./routes/AdminAgePoints.route");
-app.use("/api/admin/age-points", adminAgePointRouter);
-
-const crsEducationPointRouter = require('./routes/EducationCRSPoint.route');
-app.use("/api/crs-education", crsEducationPointRouter);
-
-const adminCRSEducationPointRouter = require('./routes/AdminEducationCRSPoint.route');
-app.use("/api/admin/crs-education", adminCRSEducationPointRouter);
-
-const canadaCRSEducation = require('./routes/CanadaCRSEducation.route');
-app.use("/api/canada/crs/education", canadaCRSEducation);
-
-const adminCanadaCRSEducation = require('./routes/AdminCanadaCRSEducation.route');
-app.use("/api/admin/canada/crs/education", adminCanadaCRSEducation);
-
-const educationPointRouter = require("./routes/EducationPoints.route");
-app.use("/api/education-points", educationPointRouter);
-
-const adminEducationPointRouter = require("./routes/AdminEducationPoints.route");
-app.use("/api/admin/education-points", adminEducationPointRouter);
-
-const examNameRouter = require("./routes/ExamName.route");
-app.use("/api/exam-name", examNameRouter);
-
-const adminExamNameRouter = require("./routes/AdminExamName.route");
-app.use("/api/admin/exam-name", adminExamNameRouter);
-
-const examScoreRouter = require("./routes/ExamScore.route");
-app.use("/api/exam-scores", examScoreRouter);
-
-const adminExamScoreRouter = require("./routes/AdminExamScore.route");
-app.use("/api/admin/exam-scores", adminExamScoreRouter);
-
-const binaryOptionScore = require("./routes/BinaryOptionScore.route");
-app.use("/api/binary-option-score", binaryOptionScore);
-
-const adminBinaryOptionScore = require("./routes/AdminBinaryOptionScore.route");
-app.use("/api/admin/binary-option-score", adminBinaryOptionScore);
 
 const scoreOptionRouter = require("./routes/ScoreOption.route");
 app.use("/api/score-options", scoreOptionRouter);
@@ -67,11 +16,17 @@ app.use("/api/score-options", scoreOptionRouter);
 const adminScoreOptionRouter = require("./routes/AdminScoreOption.route");
 app.use("/api/admin/score-options", adminScoreOptionRouter);
 
-const canadaWorkExperience = require("./routes/CanadaWorkExperience.route");
-app.use("/api/canada/experience", canadaWorkExperience);
+const examScoreRouter = require("./routes/ExamScore.route");
+app.use("/api/exam-scores", examScoreRouter);
 
-const adminCanadaWorkExperience = require("./routes/AdminCanadaWorkExperience.route");
-app.use("/api/admin/canada/experience", adminCanadaWorkExperience);
+const adminExamScoreRouter = require("./routes/AdminExamScore.route");
+app.use("/api/admin/exam-scores", adminExamScoreRouter);
+
+const examNameRouter = require("./routes/ExamName.route");
+app.use("/api/exam-name", examNameRouter);
+
+const adminExamNameRouter = require("./routes/AdminExamName.route");
+app.use("/api/admin/exam-name", adminExamNameRouter);
 
 const foreignWorkExperience = require("./routes/ForeignWorkExperience.route");
 app.use("/api/foreign/experience", foreignWorkExperience);
@@ -85,18 +40,14 @@ app.use("/api/noc-job", NOCTeerJobOfferRouter);
 const adminNOCTeerRouter = require("./routes/AdminNOCTeerJobOffer.route");
 app.use("/api/admin/noc-job", adminNOCTeerRouter);
 
+const skillTransferabilityRoute = require("./routes/skillTransferability.route")
+app.use("/api/skill-transferability", skillTransferabilityRoute);
+
 const testing = require("./routes/questionRoutes")
 app.use("/api/testing", testing);
 
-const spouseLanguage = require("./routes/SpouseLanguageMarking.route")
-app.use("/api/spouse/language", spouseLanguage);
-
-const adminSpouseLanguage = require("./routes/AdminSpouseLanguageMarking.route")
-app.use("/api/admin/spouse/language", adminSpouseLanguage);
-
-// Skill transferability factors
-const skillTransferabilityRoute = require("./routes/skillTransferability.route")
-app.use("/api/skill-transferability", skillTransferabilityRoute);
+const authRouter = require("./routes/Auth.route");
+app.use("/api/admin", authRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
